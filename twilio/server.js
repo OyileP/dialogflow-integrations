e127d1390c29117e89d513f8c09344950d375a6e
OyileP <120788307+OyileP@users.noreply.github.com>
2022-12-17 16:13:37 +0300

    Update server.js
    Changed the server credentials

--- twilio/server.js
+++ twilio/server.js
@@ -29,10 +29,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //See https://dialogflow.com/docs/reference/v2-auth-setup and
 // https://cloud.google.com/dialogflow/docs/setup for details.
 
-const projectId = 'Place your dialogflow projectId here';
-const phoneNumber = "Place your twilio phone number here";
-const accountSid = 'Place your accountSid here';
-const authToken = 'Place your authToken here';
+const projectId = 'kibu-chatbot-iojy';
+const phoneNumber = "+14155238886";
+const accountSid = 'AC2e1285aab06ee63ec5531275a154b324';
+const authToken = '056ff946803755014082ea2c930b1692';
 
 const client = require('twilio')(accountSid, authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
